4962e634613d6615da9b929cb80c1b60e3056360
Chris Perez <chris.k.perez@gmail.com>
2016-07-11 11:57:46 -0700

    auth routes and updates from schema code

--- api_server.js
+++ api_server.js
@@ -3,19 +3,23 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authRouter = require('./routes/auth_routes');
+
 const dbPort = process.env.MONGOLAB_URI || 'mongodb://localhost/dev_db';
 
 mongoose.connect(dbPort);
 
 app.use(cors());
 
+app.use('/auth', authRouter);
+
 app.use((req, res) => {
   console.log('hit end');
   res.status(404).json({message: 'route not found'});
-}).use(err, req, res, next) => {
+}).use((err, req, res, next) => {
   res.status(500).json({message: err.message});
   next(err);
-};
+});
 
 app.listen(3000, () => {
   console.log('listening on 3000, foo');
---
+++ routes/auth_routes.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser').json();
+const User = require('../model/user');
+const basicHTTP = require('../lib/basic_http');
+
+const router = module.exports = exports = express.Router();
+
+router.post('/signup', bodyParser, (req, res, next) => {
+  let freshUser = new User(req.body);
+  let hashedPassword = freshUser.hashedPassword();
+  freshUser.password = hashedPassword;
+  req.body.password = null;
+  User.findOne({username: req.body.username}, (err, user) => {
+    if (err || user) return next(new Error('Error. Someone else may have this username already.'));
+    freshUser.save((err, user) => {
+      if (err) return next(new Error('Could not save user info. Please try again.'));
+      res.json({token: user.generateToken()});
+    });
+  });
+});
+
+router.get('/signin', basicHTTP, (req, res, next) => {
+  User.findOne({username: req.auth.username}, (err, user) => {
+    if (err || !user) return next(new Error('Error logging in. Do you have an account?'));
+    if (!user.comparePassword(req.auth.password)) return next(new Error('Sorry. Looks like your password was fucky.'));
+    res.json({token: user.generateToken()});
+  });
+});
