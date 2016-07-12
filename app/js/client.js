690ab00d60f79bcd41a8405c7f42f108dbbb23a8
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-07-12 14:44:38 -0700

    routed the home page and updated gulp file

--- app/js/client.js
+++ app/js/client.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const angular = require('angular');
-//const ngRoute = require('angular-route');
+const ngRoute = require('angular-route');
 
-const app = angular.module('Pong-App', []);
+const app = angular.module('Pong-App', [ngRoute]);
 
 require('./services')(app);
 require('./controllers')(app);
 require('./directives')(app);
+
+app.config(function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: './templates/partials/home.html',
+    controller: 'AuthController',
+    controllerAs: 'authctrl'
+  });
+});
