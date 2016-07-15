8e6f5f7d3dffffc4e8549d34162a6044a066a072
simonszc <zsimonson26@gmail.com>
2016-07-14 20:01:38 -0700

    some controller tests

---
+++ fetest/controller_test.js
@@ -0,0 +1,43 @@
+const angular = require('angular');
+require('angular-mocks');
+require('../app/js/client.js');
+
+describe('controller tests', () => {
+  let authctrl;
+  let logctrl;
+  let plyrctrl;
+  let usrctrl;
+  beforeEach(()=> {
+    angular.mock.module('Pong-App');
+    angular.mock.inject(function($controller, _$httpBackend_) {
+      authctrl = new $controller('AuthController');
+      logctrl = new $controller('LogController');
+      $httpBackend = _$httpBackend_;
+    });
+  });
+  afterEach(() => {
+    $httpBackend.verifyNoOutstandingRequest();
+    $httpBackend.verifyNoOutstandingExpectation();
+  });
+  it('log controller should get logs', () => {
+    let testResponse = [{
+      winner: 'testWinner',
+      loser: 'testLoser',
+      winnerRank: 1,
+      loserRank: 2,
+      time: new Date().toString()
+    }];
+    $httpBackend.expectGET('/log')
+      .respond(200, testResponse);
+    logctrl.getLogs();
+    $httpBackend.flush();
+    expect(logctrl.logs[0].winner).toBe('testWinner');
+  });
+  it('log controller should be able to toggleLogs', () => {
+    logctrl.toggleLogs();
+    expect(!logctrl.hideLogs).toBe(true);
+  });
+  it('signup should be a function on authctrl', () => {
+    expect(typeof authctrl.signup).toBe('function');
+  })
+});
